f32208cfa6cd8d1d1ddd2d1b508807ae22aa93e1
MacBook <MacBook@MacBooks-MBP.home>
2015-06-02 21:45:03 +0100

    eadded more fields to first migrate/create table.rb

--- db/migrate/20150602203440_create_missions.rb
+++ db/migrate/20150602203440_create_missions.rb
@@ -1,6 +1,12 @@
 class CreateMissions < ActiveRecord::Migration
   def change
     create_table :missions do |t|
+      t.string :name
+      t.text :image
+      t.text :outcome
+      t.integer :mass
+      t.timestamps
+
     end
   end
 end
