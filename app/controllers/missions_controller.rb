fac8552b00fd69d51753687231097abe6648be53
MacBook <MacBook@MacBooks-MBP.home>
2015-06-02 23:37:50 +0100

    Took a long detour through documentation trying to work out how to pull information from wikipedia.  Tried a method based on the movies app we built in class but i couldnt make it work so tried different approach.
    Used gem called wikipedia-client.  Took ages to work out it also needed
    gem called wikipedia to work. Felt like i learnt alot on the way now and
    at least the example works now.  Next step is to try to pass values to the
    gem to get data back from the relevant pages.

--- app/controllers/missions_controller.rb
+++ app/controllers/missions_controller.rb
@@ -1,15 +1,23 @@
 class MissionsController < ApplicationController
   
+  # Params is talking to the database as well
+# but here we are defining what is going to happen when our 
+# functions are called.  Function is called, controller handles
+# the call or action to the database and sends back the response.
+
+
   def index
     @missions = Mission.all
   end
-
+  # this gets all this items from db as object?
   def show
     @mission = Mission.find(params[:id])
     end
-
+ # this gets the id as a param from the url and returns that row as object
   def new
     @mission = Mission.new
+
+    # make a new one
 
   end
 
@@ -37,10 +45,30 @@ class MissionsController < ApplicationController
   end
 
 
+  def wiki
+     @page = Wikipedia.find('Getting Things Done') 
+   end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     private
 
     def mission_params
-      params.require(:mission).permit(:name, :image, :mass, :moons)
+      params.require(:mission).permit(:name, :image, :outcome, :objective)
     end
 
 
